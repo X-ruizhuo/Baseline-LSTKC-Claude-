ad347687cee0773b1f80f9f915032320dcb42e9d
X-ruizhuo <15200356128@163.com>
2026-04-26 08:42:18 +0000

    格式化

--- Bi-C2R/run1.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=0 python continual_train.py --logs-dir logs-res-setting1/ --setting 1
--- Bi-C2R/run2.sh
+++
@@ -1,1 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 python continual_train.py --logs-dir logs-res-setting2/ --setting 2
